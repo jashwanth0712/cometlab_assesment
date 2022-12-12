3559b755867a802725521e68ad4502b931f3458b
jashwanth0712 <jashwanth0712@gmail.com>
2022-12-13 02:38:11 +0530

    added exception handling

--- controllers/github.js
+++ controllers/github.js
@@ -8,21 +8,27 @@ const octokit = new Octokit({ auth: API_key });
 
 export const getAllRepos= async function (req, res) {
     let username="";
-    if(!req.body.username){
-        //when username is not mentioned in the body. get request is made to fing the loginID of the authenticated account
-        const response = await octokit.request("GET /user", {
-            org: "octokit",
-            type: "private",
-            });
-        username=response.data.login;
-    }
-    else
-        username=req.body.username;
-        
-    console.log(`username that was processed is ${username}`)
-    const repos_list=await octokit.request(`GET /users/${username}/repos`, {
-        org: 'ORG',
-        data: undefined
-      })
-    res.send(repos_list.data)
+    try{
+        if(!req.body.username){
+            //when username is not mentioned in the body. get request is made to fing the loginID of the authenticated account
+            const response = await octokit.request("GET /user", {
+                org: "octokit",
+                type: "private",
+                });
+            username=response.data.login;
+        }
+        else
+            username=req.body.username;
+            
+        console.log(`username that was processed is ${username}`)
+        const repos_list=await octokit.request(`GET /users/${username}/repos`, {
+            org: 'ORG',
+            data: undefined
+          })
+        res.send(repos_list.data)
+    }catch (err) {
+		console.log(err);
+		res.status(500).json({msg: `Internal Server Error.`});
+	}
+   
 }
