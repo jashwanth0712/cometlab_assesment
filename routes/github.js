4bfcaa00b919787eb6305a3d8d866bcc7acf663f
jashwanth0712 <jashwanth0712@gmail.com>
2022-12-14 13:34:47 +0530

    added configuration for vercel

--- routes/github.js
+++ routes/github.js
@@ -2,12 +2,14 @@ import express from "express";
 import { getAllRepos ,createRepo,getRepo,Topics} from "../controllers/github.js";
 const message={
     "msg":"list of API endpoints",
-    "Endpoints":[
-        "https://cometlab-assesment.vercel.app/getAllRepos",
-        "https://cometlab-assesment.vercel.app/createRepo",
-        "https://cometlab-assesment.vercel.app/getRepo",
-        "https://cometlab-assesment.vercel.app/Topics/:method"
-    ]
+    "Endpoints":{
+        "post methods":[
+            "https://cometlab-assesment.vercel.app/getAllRepos",
+            "https://cometlab-assesment.vercel.app/createRepo",
+            "https://cometlab-assesment.vercel.app/getRepo",
+            "https://cometlab-assesment.vercel.app/Topics/:method"
+        ]
+    }
 }
 const router = express.Router();
 // https://api.github.com/users/jashwanth0712/repos
@@ -18,4 +20,9 @@ router.post("/getAllRepos", getAllRepos);
 router.post("/createRepo", createRepo);
 router.post("/getRepo", getRepo)
 router.post("/Topics/:method",Topics)
+// Default route
+app.get("*", (req, res) => {
+    res.send("PAGE NOT FOUND");
+  });
+    
 export default router
