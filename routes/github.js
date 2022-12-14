ad7f6fd0616fd3512372787055df297ce508ea38
jashwanth0712 <jashwanth0712@gmail.com>
2022-12-14 13:27:47 +0530

    added configuration for vercel

--- routes/github.js
+++ routes/github.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { getAllRepos ,createRepo,getRepo,Topics} from "../controllers/github.js";
-
+const message={
+    "msg":"list of API endpoints",
+    "Endpoints":[
+        "https://cometlab-assesment.vercel.app/getAllRepos",
+        "https://cometlab-assesment.vercel.app/createRepo",
+        "https://cometlab-assesment.vercel.app/getRepo",
+        "https://cometlab-assesment.vercel.app/Topics/:method"
+    ]
+}
 const router = express.Router();
 // https://api.github.com/users/jashwanth0712/repos
 router.get("/", async function (req, res) {
-    res.send("recieverd")
+    res.send(message)
 });
 router.post("/getAllRepos", getAllRepos);
 router.post("/createRepo", createRepo);
